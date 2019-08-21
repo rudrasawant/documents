2d4f8985ab4a7413b22e612e8b9cb8f2df66f4c9
Rudra-Sawant <rudra_sawant@persistent.co.in>
2019-08-21 14:53:36 +0530

    fixed conflicts

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,8 @@
 package app;
 public class App{
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello world of java ....we are awesome");
-=======
         System.out.println("I am bored with the training!");
->>>>>>> master
+
     }
 }
