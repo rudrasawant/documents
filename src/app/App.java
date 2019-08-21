1c7555c8d9cae63a717ec1d230d62fd96f694f17
Rudra-Sawant <rudra_sawant@persistent.co.in>
2019-08-21 16:00:07 +0530

    changes after reapplying'

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,7 @@
 package app;
 public class App{
     public static void main(String[] args) {
-        System.out.println("Hello world of java ....we are awesome");
+        System.out.println("Hello world of java ....we are awesome.....hahahhhhahh");
         System.out.println("I am bored with the training!");
 
     }
