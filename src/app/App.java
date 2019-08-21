96c7aa796b12d8836cad60fc2ec515b0d9e34831
Rudra-Sawant <rudra_sawant@persistent.co.in>
2019-08-21 14:38:45 +0530

    new print stmt addded

--- src/app/App.java
+++ src/app/App.java
@@ -2,5 +2,6 @@ package app;
 public class App{
     public static void main(String[] args) {
         System.out.println("Hello world of java ....Java is awesome");
+        System.out.println("I am bored with the training!");
     }
 }
